70dac12362568c28d1e9618199e29bb14b7bad6e
11kim <fish.are.crafty@yandex.ru>
2017-09-25 21:42:03 +0300

    hw1

--- day01/1.py
+++
@@ -1,6 +0,0 @@
-a = input()
-if not a.isdigit():
-    print("not int")
-else:
-    print("The previous number is " + str(int(a) - 1))
-    print("The next one is " + str(int(a) + 1))
---
+++ hw1/1.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if b != 0 and (a % b == c):
+    print("Property (3) is True")
+else:
+    print("Property (3) is False")
+if (a * c + b == 0):
+    print("Property (4) is True")
+else:
+    print("Property (4) is False")
