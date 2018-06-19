f9ed0491d9bdc3e37e8b39725f7c0de3e42848fd
11kim <fish.are.crafty@yandex.ru>
2018-06-19 12:30:55 +0300

    1

--- exam/ex3.py
+++ exam/ex3.py
@@ -4,8 +4,18 @@ import re
 def find_coll(filename):
     with open('news/' + filename, 'r', encoding='windows-1251') as file:
         lines = file.readlines()
+        ph = ''
         for i in range(len(lines)):
             line = lines[i].rstrip()
+            if line.startswith('<se>'):
+                ph = ''
+            else:
+                if re.findall('</ana>(.+)</w>', line):
+                    word = re.findall('</ana>(.+)</w>', line)[0]
+                    plus = ''
+                    if word[-1] in '.,():?!':
+                        plus = word[-1]
+                    ph += word  + plus + ' '
             if line.startswith('<w><ana lex=') and line.endswith('</w>') and lines[i + 1].startswith('<w><ana lex='):
                 next = lines[i + 1]
                 word1 = re.findall('</ana>(.+)</w>', line)[0]
@@ -13,8 +23,8 @@ def find_coll(filename):
                 gr1 = re.findall('gr="(.+)"', line)[0]
                 gr2 = re.findall('gr="(.+)"', next)[0]
                 if gr1[0] == 'S' and gr2[0] == 'S' and 'gen' in gr2:
-                    make_file(word1 + ' ' + word2, '')
-                    print(word1, word2)
+                    make_file(word1 + ' ' + word2, ph)
+                    print(word1, word2, ph)
 
 
 def make_file(bi, ph):
